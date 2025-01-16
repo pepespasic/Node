8997bbf98637875c77989775d337bf52d4fa5639
pepespasic <pepespasic@gmail.com>
2025-01-15 21:18:28 -0800

    Added more course files

--- reference/path_demo.js
+++ reference/path_demo.js
@@ -1,6 +1,4 @@
-// Course link: https://www.youtube.com/watch?v=fBNz5xF-Kx4
-
-const path = require('path')
+const path = require('path');
 
 // Base file name
 console.log(path.basename(__filename));
@@ -9,10 +7,39 @@ console.log(path.basename(__filename));
 console.log(path.dirname(__filename));
 
 // File extension
-console.log(path.extname(__filename)); 
+console.log(path.extname(__filename));
 
-// Create path object 
+// Create path object
 console.log(path.parse(__filename).base);
 
-// Concantenate paths
-console.log(path.join(__dirname, 'test', 'hello.html'))
+// Concatenate paths
+console.log(path.join(__dirname, 'test', 'hello.html'));
+
+
+
+// More examples
+
+
+// get the path delimiter base on the current OS Environment
+const platSpec = path.delimiter;
+
+console.log(platSpec);
+
+
+// ge the path format on POSIX : / and Windows : \
+// more info : https://nodejs.org/dist/latest-v11.x/docs/api/path.html#path_path_format_pathobject
+const pathformat = path.format({
+  dir: pathjoin,
+  root: pathjoin,
+  base: pathjoin,
+  name: pathjoin,
+  ext: pathjoin,
+});
+
+console.log(pathformat);
+
+
+// get the parent folder director
+const parentDir = path.dirname(__dirname);
+
+console.log(parentDir);
